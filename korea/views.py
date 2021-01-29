769e0e3e21b41a858465dcbd564e04b5c1e0e976
phw4483 <gusdn3477@naver.com>
2021-01-30 00:00:22 +0900

    프로젝트 최종

--- korea/views.py
+++ korea/views.py
@@ -1,35 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here
-def visit_palace(request):
-    return render(request, 'korea/palace.html')
 
-def visit_ntower(request):
-    return render(request, 'korea/ntower.html')
-
-def visit_63building(request):
-    return render(request, 'korea/63building.html')
-
-def visit_lotteworld(request):
-    return render(request, 'korea/lotteworld.html')
-
-def visit_bukchon(request):
-    return render(request, 'korea/bukchon.html')
-
-def visit_myeongdong(request):
-    return render(request, 'korea/myeongdong.html')
 
 def main(req):
     return render(req, 'korea/index.html')
-
-def section_place(req):
-    return render(req, 'korea/section_place.html')
-
-def theme_place(req):
-    return render(req, 'korea/theme_place.html')
-
-def will(req):
-    return render(req, 'korea/will.html')
 
 def hotplaces(req):
     return render(req, 'korea/hotplaces.html')
